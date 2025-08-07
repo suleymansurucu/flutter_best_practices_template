5dfc187568a950bc82e6dd4a2315348191b4f174
suleymansurucu <suleymansurucu95@gmail.com>
2025-08-07 13:54:00 -0400

    feat: Add Localization Support #1 . Create custom product location class

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_best_practices_template/product/init/product_localizaiton.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(ProductLocalization(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,23 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
---
+++ lib/product/init/product_localizaiton.dart
@@ -0,0 +1,31 @@
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_best_practices_template/product/utility/constant/enums/locales.dart';
+
+/// This class provides localization support for the application using the
+final class ProductLocalization extends EasyLocalization {
+  /// [ProductLocalization] constructor initializes the localization with
+  ProductLocalization({required super.child})
+    : super(
+        supportedLocales: _supportedLocales,
+        path: TranslationsPath,
+        useOnlyLangCode: true,
+      );
+
+  static final List<Locale> _supportedLocales = [
+    Locales.tr.locale,
+    Locales.en.locale,
+  ];
+
+  static const String TranslationsPath = 'assets/translations';
+
+  /// Updates the application's locale to the specified [locale].
+  static Future<void> updateLanguage({
+    required BuildContext context,
+    required Locales locale,
+  }) async {
+    await context.setLocale(locale.locale);
+  }
+}
